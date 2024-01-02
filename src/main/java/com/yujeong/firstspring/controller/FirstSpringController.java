9d488719008f45b2e9a01746dd14d6b15525f3a9
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-02 16:50:24 +0900

    Feat: add hello mvc in controller

--- src/main/java/com/yujeong/firstspring/controller/FirstSpringController.java
+++ src/main/java/com/yujeong/firstspring/controller/FirstSpringController.java
@@ -3,6 +3,7 @@ package com.yujeong.firstspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstSpringController {
@@ -11,5 +12,11 @@ public class FirstSpringController {
     public String hello(Model model) {
         model.addAttribute("data", "This is my first spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
