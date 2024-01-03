89d9f2d40b621c81c5fab2fb25a06a69f21c5e0c
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-03 21:44:33 +0900

    Test: add save test code

--- src/test/java/com/yujeong/firstspring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/yujeong/firstspring/repository/MemoryMemberRepositoryTest.java
@@ -1,2 +1,21 @@
-package com.yujeong.firstspring.repository;public class MemoryMemberRepositoryTest {
+package com.yujeong.firstspring.repository;
+
+import com.yujeong.firstspring.domain.Member;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
 }
