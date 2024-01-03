40fdb98ea420cd13d7f87fad78d2eb36a7e8a552
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-03 21:50:03 +0900

    Test: add findByName test code

--- src/test/java/com/yujeong/firstspring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/yujeong/firstspring/repository/MemoryMemberRepositoryTest.java
@@ -18,4 +18,19 @@ public class MemoryMemberRepositoryTest {
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
     }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
 }
