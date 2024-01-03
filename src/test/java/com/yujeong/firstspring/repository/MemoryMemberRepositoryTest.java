2b12d507615212db99af5f3e3bdf9d8c9cdbc411
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-03 22:03:29 +0900

    Test: add clear store

--- src/test/java/com/yujeong/firstspring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/yujeong/firstspring/repository/MemoryMemberRepositoryTest.java
@@ -2,13 +2,20 @@ package com.yujeong.firstspring.repository;
 
 import com.yujeong.firstspring.domain.Member;
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 public class MemoryMemberRepositoryTest {
 
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
 
     @Test
     public void save() {
