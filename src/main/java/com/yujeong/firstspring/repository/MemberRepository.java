523ee36a8b42dd5e5c313cbe91d0ae66d13cdd49
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-03 21:41:09 +0900

    Feat: add member repoitory interface

--- src/main/java/com/yujeong/firstspring/repository/MemberRepository.java
+++ src/main/java/com/yujeong/firstspring/repository/MemberRepository.java
@@ -1,2 +1,13 @@
-package com.yujeong.firstspring.repository;public interface MemberRepository {
+package com.yujeong.firstspring.repository;
+
+import com.yujeong.firstspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
 }
