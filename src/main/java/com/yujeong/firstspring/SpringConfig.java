5ac910c891e9d316b877a18ace00b530f87b4d0e
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-05 17:13:28 +0900

    Feat: add spring config

--- src/main/java/com/yujeong/firstspring/SpringConfig.java
+++ src/main/java/com/yujeong/firstspring/SpringConfig.java
@@ -1,2 +1,21 @@
-package com.yujeong.firstspring;public class SpringConfig {
+package com.yujeong.firstspring;
+
+import com.yujeong.firstspring.repository.MemberRepository;
+import com.yujeong.firstspring.repository.MemoryMemberRepository;
+import com.yujeong.firstspring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
