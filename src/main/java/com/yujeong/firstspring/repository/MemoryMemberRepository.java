92df8d8e270ef4bfd50e9896bb3787de895b13f8
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-05 15:37:10 +0900

    Chore: add spring bin with component scan

--- src/main/java/com/yujeong/firstspring/repository/MemoryMemberRepository.java
+++ src/main/java/com/yujeong/firstspring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.yujeong.firstspring.repository;
 
 import com.yujeong.firstspring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
