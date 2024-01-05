3896b03166c34b727ac70b76679d5844453b6f1f
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-05 15:36:42 +0900

    Chore: add spring bin with component scan

---
+++ src/main/java/com/yujeong/firstspring/controller/MemberController.java
@@ -0,0 +1,2 @@
+package com.yujeong.firstspring.controller;public class MemberController {
+}
--- src/main/java/com/yujeong/firstspring/service/MemberService.java
+++ src/main/java/com/yujeong/firstspring/service/MemberService.java
@@ -3,14 +3,18 @@ package com.yujeong.firstspring.service;
 import com.yujeong.firstspring.domain.Member;
 import com.yujeong.firstspring.repository.MemberRepository;
 import com.yujeong.firstspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemoryMemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
