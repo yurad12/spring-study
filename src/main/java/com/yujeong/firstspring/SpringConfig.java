94ee48fc9eb7ffefbe1fa9e15a1f7bd3d0e8c1a0
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-06 00:14:47 +0900

    Refactor: switch to jdbc member repository

--- src/main/java/com/yujeong/firstspring/SpringConfig.java
+++ src/main/java/com/yujeong/firstspring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.yujeong.firstspring;
 
+import com.yujeong.firstspring.repository.JdbcMemberRepository;
 import com.yujeong.firstspring.repository.MemberRepository;
 import com.yujeong.firstspring.repository.MemoryMemberRepository;
 import com.yujeong.firstspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
