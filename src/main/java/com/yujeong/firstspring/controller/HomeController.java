242ab66cd118ef3b6de322c8aa2f3fb3b42813b2
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-05 17:16:07 +0900

    Feat: add home controller

--- src/main/java/com/yujeong/firstspring/controller/HomeController.java
+++ src/main/java/com/yujeong/firstspring/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.yujeong.firstspring.controller;public class HomeController {
+package com.yujeong.firstspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
