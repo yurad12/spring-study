0012bc93e21b8f01ecb323c752fd81778fb2a6f8
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-05 17:39:17 +0900

    Feat: add member list api

--- src/main/java/com/yujeong/firstspring/controller/MemberController.java
+++ src/main/java/com/yujeong/firstspring/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.yujeong.firstspring.domain.Member;
 import com.yujeong.firstspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,6 +32,13 @@ public class MemberController {
 
         memberService.join(member);
 
-        return "redirect:/";
+        return "redirect:/ ";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
