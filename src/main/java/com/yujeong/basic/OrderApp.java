3ffdf3814d46846b5f1015937ee0be0ad63978b6
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-08 20:55:08 +0900

    Feat: add order app

--- src/main/java/com/yujeong/basic/OrderApp.java
+++ src/main/java/com/yujeong/basic/OrderApp.java
@@ -1,2 +1,24 @@
-package com.yujeong.basic;public class OrderApp {
+package com.yujeong.basic;
+
+import com.yujeong.basic.discount.OrderService;
+import com.yujeong.basic.discount.OrderServiceImpl;
+import com.yujeong.basic.member.Grade;
+import com.yujeong.basic.member.Member;
+import com.yujeong.basic.member.MemberService;
+import com.yujeong.basic.member.MemberServiceImpl;
+import com.yujeong.basic.order.Order;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+    }
 }
