bdefddf5d508b7b6112f0c389234526630eabd5b
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-08 20:53:28 +0900

    Feat: add order service interface

--- src/main/java/com/yujeong/basic/discount/OrderService.java
+++ src/main/java/com/yujeong/basic/discount/OrderService.java
@@ -1,2 +1,7 @@
-package com.yujeong.basic.discount;public interface OrderService {
+package com.yujeong.basic.discount;
+
+import com.yujeong.basic.order.Order;
+
+public interface OrderService {
+    Order createOrder(Long memberId, String itemName, int itemPrice);
 }
