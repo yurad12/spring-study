2cd6ff0c99f5274caeb427c136e500303d090586
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-08 20:23:42 +0900

    initial commit

---
+++ src/test/java/com/yujeong/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujeong.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
