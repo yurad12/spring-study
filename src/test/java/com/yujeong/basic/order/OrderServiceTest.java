4ca01e956a6714f7aeda83ad1357746f743399a8
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-08 20:55:22 +0900

    Test: add order service test

--- src/test/java/com/yujeong/basic/order/OrderServiceTest.java
+++ src/test/java/com/yujeong/basic/order/OrderServiceTest.java
@@ -1,2 +1,26 @@
-package com.yujeong.basic.order;public class OrderServiceTest {
+package com.yujeong.basic.order;
+
+import com.yujeong.basic.discount.OrderService;
+import com.yujeong.basic.discount.OrderServiceImpl;
+import com.yujeong.basic.member.Grade;
+import com.yujeong.basic.member.Member;
+import com.yujeong.basic.member.MemberService;
+import com.yujeong.basic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
 }
