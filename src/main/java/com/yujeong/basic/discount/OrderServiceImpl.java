462cef76c78b1fb1775ed00f7c9e8502325a8342
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-08 20:53:44 +0900

    Feat: add implementation of order service

--- src/main/java/com/yujeong/basic/discount/OrderServiceImpl.java
+++ src/main/java/com/yujeong/basic/discount/OrderServiceImpl.java
@@ -1,2 +1,20 @@
-package com.yujeong.basic.discount;public class OrderServiceImpl {
+package com.yujeong.basic.discount;
+
+import com.yujeong.basic.member.Member;
+import com.yujeong.basic.member.MemberRepository;
+import com.yujeong.basic.member.MemoryMemberRepository;
+import com.yujeong.basic.order.Order;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
 }
