09b989314ee9e6750f3bd2465bb9c51661d2ec12
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-08 20:53:01 +0900

    Feat: add implementation of discount policy

--- src/main/java/com/yujeong/basic/discount/FixDiscountPolicy.java
+++ src/main/java/com/yujeong/basic/discount/FixDiscountPolicy.java
@@ -1,2 +1,18 @@
-package com.yujeong.basic.discount;public class FixDiscountPolicy {
+package com.yujeong.basic.discount;
+
+import com.yujeong.basic.member.Grade;
+import com.yujeong.basic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; //1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
 }
