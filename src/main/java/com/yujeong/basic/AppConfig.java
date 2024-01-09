6d680ca96a7c0ac62f2ff666f8e6e55c384c63bf
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-09 23:50:10 +0900

    Feat: add app config

--- src/main/java/com/yujeong/basic/AppConfig.java
+++ src/main/java/com/yujeong/basic/AppConfig.java
@@ -1,2 +1,22 @@
-package com.yujeong.basic;public class AppConfig {
+package com.yujeong.basic;
+
+import com.yujeong.basic.discount.FixDiscountPolicy;
+import com.yujeong.basic.discount.OrderService;
+import com.yujeong.basic.discount.OrderServiceImpl;
+import com.yujeong.basic.member.MemberService;
+import com.yujeong.basic.member.MemberServiceImpl;
+import com.yujeong.basic.member.MemoryMemberRepository;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                new MemoryMemberRepository(),
+                new FixDiscountPolicy()
+        );
+    }
 }
