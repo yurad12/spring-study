0289702ef76a2ce8851d3cca51373f95e318dd05
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-09 23:49:57 +0900

    Refactor: implement DI in order service impl

--- src/main/java/com/yujeong/basic/discount/OrderServiceImpl.java
+++ src/main/java/com/yujeong/basic/discount/OrderServiceImpl.java
@@ -7,8 +7,13 @@ import com.yujeong.basic.order.Order;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private  final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
