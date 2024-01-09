7706d696ce33e8d46a87fbe330b8d1343d6f1761
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-10 00:49:33 +0900

    Refactor: adopt spring container in order app

--- src/main/java/com/yujeong/basic/OrderApp.java
+++ src/main/java/com/yujeong/basic/OrderApp.java
@@ -1,20 +1,20 @@
 package com.yujeong.basic;
 
 import com.yujeong.basic.discount.OrderService;
-import com.yujeong.basic.discount.OrderServiceImpl;
 import com.yujeong.basic.member.Grade;
 import com.yujeong.basic.member.Member;
 import com.yujeong.basic.member.MemberService;
-import com.yujeong.basic.member.MemberServiceImpl;
 import com.yujeong.basic.order.Order;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
