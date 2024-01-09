aec6769212d0344b0aa04eaba13935fd8cdefe60
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-10 00:49:09 +0900

    Refactor: adopt spring container in member app

--- src/main/java/com/yujeong/basic/MemberApp.java
+++ src/main/java/com/yujeong/basic/MemberApp.java
@@ -3,13 +3,16 @@ package com.yujeong.basic;
 import com.yujeong.basic.member.Grade;
 import com.yujeong.basic.member.Member;
 import com.yujeong.basic.member.MemberService;
-import com.yujeong.basic.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
