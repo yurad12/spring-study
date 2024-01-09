d28fd25e55c24b97b9e3561e1b78817707ad81d3
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-10 00:03:49 +0900

    Refactor: remove duplicaton and specify role-implementations

--- src/main/java/com/yujeong/basic/AppConfig.java
+++ src/main/java/com/yujeong/basic/AppConfig.java
@@ -1,5 +1,6 @@
 package com.yujeong.basic;
 
+import com.yujeong.basic.discount.DiscountPolicy;
 import com.yujeong.basic.discount.FixDiscountPolicy;
 import com.yujeong.basic.discount.OrderService;
 import com.yujeong.basic.discount.OrderServiceImpl;
@@ -10,13 +11,21 @@ import com.yujeong.basic.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
