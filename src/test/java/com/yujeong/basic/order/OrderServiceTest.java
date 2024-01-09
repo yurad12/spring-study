9a967167c10358414b63588404b84b735d4ca2b1
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-09 23:52:50 +0900

    Test: add before each

--- src/test/java/com/yujeong/basic/order/OrderServiceTest.java
+++ src/test/java/com/yujeong/basic/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package com.yujeong.basic.order;
 
+import com.yujeong.basic.AppConfig;
 import com.yujeong.basic.discount.OrderService;
 import com.yujeong.basic.discount.OrderServiceImpl;
 import com.yujeong.basic.member.Grade;
@@ -7,12 +8,20 @@ import com.yujeong.basic.member.Member;
 import com.yujeong.basic.member.MemberService;
 import com.yujeong.basic.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
