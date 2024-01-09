4c70ebe04374d496f51d41a71fc17b2ec598610c
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-10 00:50:41 +0900

    Refactor: switch to spring container based

--- src/main/java/com/yujeong/basic/AppConfig.java
+++ src/main/java/com/yujeong/basic/AppConfig.java
@@ -4,17 +4,23 @@ import com.yujeong.basic.discount.*;
 import com.yujeong.basic.member.MemberService;
 import com.yujeong.basic.member.MemberServiceImpl;
 import com.yujeong.basic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     private static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
@@ -22,6 +28,7 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
