f70befbe9903e412423126a1a4afdd29dcc26286
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-09 23:08:06 +0900

    Feat: add rate discount policy

---
+++ src/main/java/com/yujeong/basic/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.yujeong.basic.discount;
+
+import com.yujeong.basic.member.Grade;
+import com.yujeong.basic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
