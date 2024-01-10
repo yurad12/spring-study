826139001e8c9d1f1e636a96a26622e15623f0b8
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-10 20:37:14 +0900

    Test: add pure container test

---
+++ src/test/java/com/yujeong/basic/singleton/SingletonService.java
@@ -0,0 +1,2 @@
+package com.yujeong.basic.singleton;public class SingletonService {
+}
---
+++ src/test/java/com/yujeong/basic/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package com.yujeong.basic.singleton;
+
+import com.yujeong.basic.AppConfig;
+import com.yujeong.basic.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
