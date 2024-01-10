a63cce90dd023765e2249bacaff63d9b5ef9a159
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-10 19:50:37 +0900

    Test: add xml app config test

--- src/test/java/com/yujeong/basic/xml/XmlAppContext.java
+++ src/test/java/com/yujeong/basic/xml/XmlAppContext.java
@@ -1,2 +1,18 @@
-package com.yujeong.basic.xml;public class XmlAppContext {
+package com.yujeong.basic.xml;
+
+import com.yujeong.basic.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberSerivce = ac.getBean("memberService", MemberService.class);
+        assertThat(memberSerivce).isInstanceOf(MemberService.class);
+    }
 }
