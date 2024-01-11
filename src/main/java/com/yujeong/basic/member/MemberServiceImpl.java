cad3fc0c9ae925064a7e51559a34b3803883b29b
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-11 19:04:18 +0900

    Refactor: delete useless import

--- src/main/java/com/yujeong/basic/member/MemberServiceImpl.java
+++ src/main/java/com/yujeong/basic/member/MemberServiceImpl.java
@@ -1,7 +1,6 @@
 package com.yujeong.basic.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
