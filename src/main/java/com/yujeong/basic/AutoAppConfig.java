553e826bfcf2a5640fa3ae8f48efdb48971b6fdf
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-11 19:05:37 +0900

    Refactor: delete base packages option

--- src/main/java/com/yujeong/basic/AutoAppConfig.java
+++ src/main/java/com/yujeong/basic/AutoAppConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "com.yujeong.basic.member",
-        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
