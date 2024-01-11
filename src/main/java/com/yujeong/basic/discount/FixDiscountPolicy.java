db8f450025e15ee01fa9b08079591985d4b5024f
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-11 19:35:04 +0900

    Refactor: add component

--- src/main/java/com/yujeong/basic/discount/FixDiscountPolicy.java
+++ src/main/java/com/yujeong/basic/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.yujeong.basic.discount;
 
 import com.yujeong.basic.member.Grade;
 import com.yujeong.basic.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; //1000원 할인
