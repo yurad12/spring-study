1872849f4f97a71bf3c08ccfdd35353f2825e89a
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-11 16:25:09 +0900

    Feat: add auto app config

--- src/main/java/com/yujeong/basic/AutoAppConfig.java
+++ src/main/java/com/yujeong/basic/AutoAppConfig.java
@@ -1,2 +1,14 @@
-package com.yujeong.basic;public class AutoAppConfig {
+package com.yujeong.basic;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "com.yujeong.basic.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
 }
