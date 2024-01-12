1bf50760c9365f6f6d164c9c6c03b116235490e5
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-12 16:35:41 +0900

    Refactor: use jsr-330 provider

--- src/test/java/com/yujeong/basic/scope/SingletonWithPrototypeTest.java
+++ src/test/java/com/yujeong/basic/scope/SingletonWithPrototypeTest.java
@@ -2,6 +2,7 @@ package com.yujeong.basic.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,33 +13,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SingletonWithPrototypeTest {
 
     @Test
-    void prototypeFind() {
+    void singletonClientUserPrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        int count2 = clientBean1.logic();
-        assertThat(count2).isEqualTo(2);
-    }
-
-    @Test
-    void singletonClientUserPrototype() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean; //생성시점에 주입
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
