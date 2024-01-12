18667b5b448fc479bb4f831f552099f36b9eb661
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-12 15:30:02 +0900

    Refactor: use postconstruct and predestroy

--- src/test/java/com/yujeong/basic/lifecycle/NetworkClient.java
+++ src/test/java/com/yujeong/basic/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package com.yujeong.basic.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,12 +29,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NeworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NeworkClient.close");
         disConnect();
