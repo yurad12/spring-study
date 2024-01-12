0811cabbfc78e4446524d80da73e533b30149ac2
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-12 14:57:11 +0900

    Test: add bean life cycle test

--- src/test/java/com/yujeong/basic/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/yujeong/basic/lifecycle/BeanLifeCycleTest.java
@@ -1,2 +1,28 @@
-package com.yujeong.basic.lifecycle;public class BeanLifeCycleTest {
+package com.yujeong.basic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
 }
