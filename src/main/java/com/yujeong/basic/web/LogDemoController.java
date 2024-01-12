a9bf88b0ee941cc609115ddeefad31d5626297ad
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-12 18:34:32 +0900

    Feat: add log demo controller

--- src/main/java/com/yujeong/basic/web/LogDemoController.java
+++ src/main/java/com/yujeong/basic/web/LogDemoController.java
@@ -1,2 +1,27 @@
-package com.yujeong.basic.web;public class LogDemoController {
+package com.yujeong.basic.web;
+
+import com.yujeong.basic.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
 }
