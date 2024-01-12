5e59dfecb8739c541058fbb702267d3a281c2acb
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-12 15:02:16 +0900

    Refactor: use initializingbean and disposablebean

--- src/test/java/com/yujeong/basic/lifecycle/NetworkClient.java
+++ src/test/java/com/yujeong/basic/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package com.yujeong.basic.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,4 +29,15 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NeworkClient.destroy");
+        disconnect();
+    }
 }
