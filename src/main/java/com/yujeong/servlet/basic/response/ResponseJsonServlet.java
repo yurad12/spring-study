2b831aeda9fea168cad7d9f207eba596f7270660
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-15 22:08:28 +0900

    Feat: add response json servlet

---
+++ src/main/java/com/yujeong/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,32 @@
+package com.yujeong.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yujeong.servlet.basic.HelloData;
+import com.yujeong.servlet.basic.HelloServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HelloServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        String result = objectMapper.writeValueAsString(helloData);
+
+        response.getWriter().write(result);
+    }
+}
