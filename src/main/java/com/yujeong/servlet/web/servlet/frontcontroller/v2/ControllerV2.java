ca38c1e822bf8d8dd51bcd1c9c6b7d3bbe8582af
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-18 16:49:42 +0900

    Feat: add myview view

---
+++ src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,2 @@
+package com.yujeong.servlet.web.servlet.frontcontroller.v2;public interface ControllerV2 {
+}
---
+++ src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,2 @@
+package com.yujeong.servlet.web.servlet.frontcontroller.v2.controller;public class MemberListControllerV2 {
+}
