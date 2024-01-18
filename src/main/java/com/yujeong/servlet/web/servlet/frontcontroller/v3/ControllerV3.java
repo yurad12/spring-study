58773c5cc28923d153f2bfdd71f79478f79aff4e
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-18 17:37:07 +0900

    Feat: add controller v3 interface

--- src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v3/ControllerV3.java
+++ src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v3/ControllerV3.java
@@ -1,2 +1,10 @@
-package com.yujeong.servlet.web.servlet.frontcontroller.v3;public interface ControllerV3 {
+package com.yujeong.servlet.web.servlet.frontcontroller.v3;
+
+import com.yujeong.servlet.web.servlet.frontcontroller.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+
+    ModelView process(Map<String, String> paramMap);
 }
