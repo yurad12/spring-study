9b550e823b2ba8a3d202bc05571292a297cac0b5
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-18 16:50:58 +0900

    Feat: add member save controller v2

--- src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -1,2 +1,31 @@
-package com.yujeong.servlet.web.servlet.frontcontroller.v2.controller;public class MemberSaveControllerV2 {
+package com.yujeong.servlet.web.servlet.frontcontroller.v2.controller;
+
+import com.yujeong.servlet.domain.member.Member;
+import com.yujeong.servlet.domain.member.MemberRepository;
+import com.yujeong.servlet.web.servlet.frontcontroller.MyView;
+import com.yujeong.servlet.web.servlet.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        request.setAttribute("member", member);
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
 }
