f05d765fd12362f88b4b26cb5b984997853fbb4b
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-18 19:22:35 +0900

    Feat: add my handler adapter interface

--- src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v5/MyHandlerAdapter.java
@@ -1,2 +1,15 @@
-package com.yujeong.servlet.web.servlet.frontcontroller.v5;public class MyHandlerAdapter {
+package com.yujeong.servlet.web.servlet.frontcontroller.v5;
+
+import com.yujeong.servlet.web.servlet.frontcontroller.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
 }
---
+++ src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,2 @@
+package com.yujeong.servlet.web.servlet.frontcontroller.v5.adapter;public class ControllerV3HandlerAdapter {
+}
