89b70b445e2bdb37f1ec5016379dbcf82331e791
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-18 17:37:30 +0900

    Feat: add member form controller v3

--- src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ src/main/java/com/yujeong/servlet/web/servlet/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -1,2 +1,14 @@
-package com.yujeong.servlet.web.servlet.frontcontroller.v3.controller;public class MemberFormControllerV3 {
+package com.yujeong.servlet.web.servlet.frontcontroller.v3.controller;
+
+import com.yujeong.servlet.web.servlet.frontcontroller.ModelView;
+import com.yujeong.servlet.web.servlet.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form");
+    }
 }
