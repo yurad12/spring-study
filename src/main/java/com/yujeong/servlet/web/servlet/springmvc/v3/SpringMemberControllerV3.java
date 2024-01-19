7fc6396ace1fb76b6aa959f5d529259ab9b7913d
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-19 17:10:06 +0900

    Refactor: use @GetMapping and @PostMapping

--- src/main/java/com/yujeong/servlet/web/servlet/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/com/yujeong/servlet/web/servlet/springmvc/v3/SpringMemberControllerV3.java
@@ -4,9 +4,7 @@ import com.yujeong.servlet.domain.member.Member;
 import com.yujeong.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,12 +14,12 @@ public class SpringMemberControllerV3 {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping(value = "/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping(value = "/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
@@ -34,7 +32,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String members(Model model) {
         List<Member> members = memberRepository.findAll();
 
