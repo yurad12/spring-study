c8d0f5b77c4ee729ad9cccf83026d7456e0e7618
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-19 17:07:08 +0900

    Feat: add integrated member controller v3 using r@RequestParam

--- src/main/java/com/yujeong/servlet/web/servlet/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/com/yujeong/servlet/web/servlet/springmvc/v3/SpringMemberControllerV3.java
@@ -1,2 +1,43 @@
-package com.yujeong.servlet.web.servlet.springmvc.v3;public class SpringMemberControllerV3 {
+package com.yujeong.servlet.web.servlet.springmvc.v3;
+
+import com.yujeong.servlet.domain.member.Member;
+import com.yujeong.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @RequestMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    @RequestMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+        return "members";
+    }
 }
