f5a180f4f923c1478130a0146c5a3403b8661e28
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-19 16:18:39 +0900

    Feat: add member list controller v1 using @Controller

--- src/main/java/com/yujeong/servlet/web/servlet/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/com/yujeong/servlet/web/servlet/springmvc/v1/SpringMemberListControllerV1.java
@@ -1,2 +1,26 @@
-package com.yujeong.servlet.web.servlet.springmvc.v1;public class SpringMemberListControllerV1 {
+package com.yujeong.servlet.web.servlet.springmvc.v1;
+
+import com.yujeong.servlet.domain.member.Member;
+import com.yujeong.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
 }
