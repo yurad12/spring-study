c7bd2e00721b3e58c2e44fe551b44a29956d82bb
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-19 16:17:56 +0900

    Feat: add member save controller v1 using @Controller

--- src/main/java/com/yujeong/servlet/web/servlet/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/com/yujeong/servlet/web/servlet/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -1,2 +1,30 @@
-package com.yujeong.servlet.web.servlet.springmvc.v1;public class SpringMemberSaveControllerV1 {
+package com.yujeong.servlet.web.servlet.springmvc.v1;
+
+import com.yujeong.servlet.domain.member.Member;
+import com.yujeong.servlet.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+
+        return mv;
+    }
 }
