1d5c3a553d38865289d3559ab763749b42517b84
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-23 13:30:06 +0900

    Feat: add request param v2 api

--- src/main/java/com/yujeong/springmvc/basic/request/RequestParamController.java
+++ src/main/java/com/yujeong/springmvc/basic/request/RequestParamController.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -19,5 +21,15 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+        @RequestParam("username") String memberName,
+        @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
     }
 }
