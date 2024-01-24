0962a362ff6248b6437adfec7f2b5ce0d1de01fb
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-24 12:36:06 +0900

    Feat: add request body json v4

--- src/main/java/com/yujeong/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/com/yujeong/springmvc/basic/request/RequestBodyJsonController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,14 @@ public class RequestBodyJsonController {
     @PostMapping("/request-body-json-v3")
     public String requestBodyJsonV3(@RequestBody HelloData helloData) {
 
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) {
+        HelloData data = httpEntity.getBody();
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
