7a231d12bb3455eb5b33cba0889a34c1bdb5a3c3
Yujeong Jeong <jjungyu1282@gmail.com>
2024-01-24 12:49:33 +0900

    Feat: add response view controller

--- src/main/java/com/yujeong/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/com/yujeong/springmvc/basic/response/ResponseViewController.java
@@ -1,2 +1,17 @@
-package com.yujeong.springmvc.basic.response;public class ResponseViewController {
+package com.yujeong.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
 }
